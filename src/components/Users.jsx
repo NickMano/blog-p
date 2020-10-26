2e9265ef3544d4febe828d901bf6473b92332d5b
Nico <nmanograsso@gmail.com>
2020-10-25 22:12:56 -0300

    Add component with table

---
+++ src/components/Users.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable react/no-array-index-key */
+import React, { useState, useEffect } from 'react';
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    setUsers(data);
+  }, []);
+
+  return (
+    <div className="margen">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Enlace</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.website}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Users;
