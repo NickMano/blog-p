538a4ecffec804f4264dc38cde54abb698dd358d
Nico <nmanograsso@gmail.com>
2020-10-31 22:37:47 -0300

    Add middleware

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import './index.css';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import reducers from './reducers';
+import App from './components/App';
+import './index.css';
 
-const store = createStore(reducers, {});
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  {},
+  composeEnhancers(applyMiddleware(reduxThunk)),
+);
 
 ReactDOM.render(
   <Provider store={store}>
