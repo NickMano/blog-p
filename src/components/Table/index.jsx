e28fa6cbf0ac5d7a1b6883f68b2748a9ba98341c
Nico <nmanograsso@gmail.com>
2020-11-01 15:40:26 -0300

    add cssClass to Table

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Table = (props) => {
-  const { headers, body } = props;
+  const { headers, body, cssClassName } = props;
   return (
-    <table>
+    <table className={cssClassName}>
       <thead>
         <tr>
-          {headers.map((header) => (<th>{header}</th>))}
+          {headers.map((header) => (<th key={header}>{header}</th>))}
         </tr>
       </thead>
       <tbody>
         {body.map((item, index) => (
           <tr key={item.id}>
             <td>{item.name}</td>
-            <td>{item.email}</td>
             <td>{item.website}</td>
-            <td>{index}</td>
+            <td><Link to={`posts/${index}`} className="arrow-right icon" /></td>
           </tr>
         ))}
       </tbody>
@@ -26,7 +26,8 @@ const Table = (props) => {
 
 Table.propTypes = {
   headers: PropTypes.arrayOf(PropTypes.string).isRequired,
-  body: PropTypes.string.isRequired,
+  body: PropTypes.arrayOf(PropTypes.object).isRequired,
+  cssClassName: PropTypes.string.isRequired,
 };
 
 export default Table;
