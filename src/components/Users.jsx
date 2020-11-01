daf2b92833ca686e047a8b8c1dbbf5a090057c05
Nico <nmanograsso@gmail.com>
2020-10-31 22:40:55 -0300

    Add loading component

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,16 +1,22 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import Loading from './Loading/Loading';
 import getAllUsers from '../actions/usersAction';
 
 const Users = (props) => {
   const [users, setUsers] = useState([]);
+  const { loading, users } = props;
 
   useEffect(() => {
     props.getAllUsers();
     const data = await response.json();
     setUsers(data);
   }, []);
-
+  if (loading) {
+    return (
+      <Loading />
+    );
+  }
   return (
     <div className="margen">
       <table className="table">
